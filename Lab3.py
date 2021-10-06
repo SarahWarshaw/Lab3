9445db95dcd0cacdab9f89cb9e9598623b4a72fd
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-10-06 20:33:08 +0000

    test

--- Lab3.py
+++ Lab3.py
@@ -6,9 +6,10 @@ myJoystick = Joystick(0x48)
 
 while True:
   try:
+    print("test")
     X = myJoystick.getX
     Y = myJoystick.getY
   except KeyboardInterrupt:
     print('Ending')
-  print(X)
+  print(int(X))
   sleep(0.1)
